01d7ed40b47633a2f5e1392b6ea6030dd5af7b75
cdrange <2257333569@qq.com>
2024-04-06 16:33:11 +0800

    青浦机厅

--- src/plugins/rangebot_others.py
+++ src/plugins/rangebot_others.py
@@ -73,14 +73,15 @@ async def _capoo(event: Event, message: Message = CommandArg()):
 
 
 """-----------机厅几(开发中)-----------"""
-jt_sh = ['qy','bl','hsh','tyg','sjhc','sjhm','wdc','wdm']
-jt_sh.extend(['wd','sjh'])
+jt_sh = ['qy','bl','hsh','tyg','sjhc','sjhm','wdc','wdm','wd','sjh']
+jt_qp = ['tc','jwh']
 jt_zb = ['yt','rs','wy','zc','wxh','sm']
 jt_jn = ['lw']
 group_sh = ['751302572','775613195','780012208','742829700','646232811','698699856','606964743']
 group_sh.extend(MAIN_GROUPS)
 group_zb = ['419134739','610587231',TEST_GROUP]
 group_jn = ['784593881',TEST_GROUP]
+group_qp = ['956305246']
 
 jtregex_warn = r"(?i)^(sjh|wd)(j|几|[0-9]+|([\+＋\-－])(\d+))$"
 jtwarn = on_regex(jtregex_warn, priority = PRIORITY_BASE*1-1, block = True)
@@ -92,14 +93,15 @@ async def _jtwarn(event: Event):
             or (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_jn) and (msg[0] in jt_jn):
         await jtwarn.finish(f"{msg[0]}同时有maimai和chunithm\n使用{msg[0]}m上报maimai人数\n使用{msg[0]}c上报chunithm人数。")
 
-jtregex = r"(?i)^(qy|bl|hsh|yt|rs|wy|lw|zc|wxh|sm|tyg|sjhc|sjhm|wdc|wdm)(j|几|[0-9]+)$"
+jtregex = r"(?i)^(qy|bl|hsh|yt|rs|wy|lw|zc|wxh|sm|tyg|sjhc|sjhm|wdc|wdm|tc|jwh)(j|几|[0-9]+)$"
 jtj = on_regex(jtregex, priority = PRIORITY_BASE*1, block = True)
 @jtj.handle()
 async def _jtj(event: Event):
     msg = re.match(jtregex,str(event.get_message()).strip().lower()).groups()
-    if (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_sh) and (msg[0] in jt_sh) \
-        or (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_zb) and (msg[0] in jt_zb)\
-            or (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_jn) and (msg[0] in jt_jn):
+    if    (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_sh) and (msg[0] in jt_sh)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_zb) and (msg[0] in jt_zb)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_jn) and (msg[0] in jt_jn)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_qp) and (msg[0] in jt_qp):
         if (8 < int(time.strftime("%H")) < 23):
             user = str(event.get_user_id())
             if msg[1] in ["j","几"]:
@@ -147,14 +149,15 @@ async def _jtj(event: Event):
         else:
             await jtj.finish("看看几点了")
 
-jtaddre = r"(?i)^(qy|bl|hsh|yt|rs|wy|lw|zc|wxh|sm|tyg|sjhc|sjhm|wdc|wdm)([\+＋\-－])(\d+)$"
+jtaddre = r"(?i)^(qy|bl|hsh|yt|rs|wy|lw|zc|wxh|sm|tyg|sjhc|sjhm|wdc|wdm|tc|jwh)([\+＋\-－])(\d+)$"
 jtadd = on_regex(jtaddre, priority = PRIORITY_BASE*1, block = True)
 @jtadd.handle()
 async def _jtadd(event: Event):
     msg = re.match(jtaddre,str(event.get_message()).strip().lower()).groups()
-    if (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_sh) and (msg[0] in jt_sh) \
-        or (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_zb) and (msg[0] in jt_zb)\
-            or (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_jn) and (msg[0] in jt_jn):
+    if    (str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_sh) and (msg[0] in jt_sh)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_zb) and (msg[0] in jt_zb)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_jn) and (msg[0] in jt_jn)\
+        or(str(re.match("group_(.+)_(.+)",event.get_session_id()).groups()[0]) in group_qp) and (msg[0] in jt_qp):
         if (8 < int(time.strftime("%H")) < 23):
             user = str(event.get_user_id())
             jtjson = readjson(static_dir + "jt/" + msg[0] + ".json")
